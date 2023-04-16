db8f73a2f3b93d6447f8bee664c3c5a7914623da
mtw72 <123168693+mtw72@users.noreply.github.com>
2023-04-16 20:12:29 +0100

    Add files via upload

---
+++ scripts/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function () {
+    // hide the loader
+    document.querySelector('.loader-container').style.display = 'none';
+    // show the content
+    document.querySelector('.content').classList.remove('hidden');
+  });
